ecbc349df6371343f77ce55cc40131a2aa4217be
laterkhaoula <laterkhaoula@gmail.com>
2026-09-11 10:27:18 +0100

    feat(candidature): add companyIndex to list candidatures for authenticated company's offers

--- app/Http/Controllers/CandidatureController.php
+++ app/Http/Controllers/CandidatureController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Offre;
 use App\Models\Candidature;
+use App\Models\CompanyProfile;
 
 class CandidatureController extends Controller
 {
@@ -27,6 +28,34 @@ class CandidatureController extends Controller
             ->get();
 
         return view('candidatures.index', compact('candidatures'));
+    }
+
+    /**
+     * Display candidatures received for the authenticated company's offers.
+     */
+    public function companyIndex(Request $request)
+    {
+        $user = $request->user();
+
+        // get company profile ids for this user
+        $companyProfileIds = $user->companyProfiles()->pluck('id')->toArray();
+
+        if (empty($companyProfileIds)) {
+            return back()->withErrors(['company' => 'Profil entreprise introuvable.']);
+        }
+
+        // get offer ids belonging to these company profiles
+        $offreIds = Offre::whereIn('profil_entreprise_id', $companyProfileIds)->pluck('id')->toArray();
+
+        // fetch candidatures for those offers only
+        $candidatures = Candidature::with(['offre' => function ($q) {
+            $q->select('id', 'titre', 'domaine', 'localisation', 'profil_entreprise_id');
+        }, 'studentProfile'])
+            ->whereIn('offre_id', $offreIds)
+            ->orderByDesc('date_candidature')
+            ->get();
+
+        return view('candidatures.company_index', compact('candidatures'));
     }
     /**
      * Store a newly created candidature for an offer by the authenticated student.
